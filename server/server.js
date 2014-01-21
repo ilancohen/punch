f771ffe19fbb240801df5ac8bcf6e30d10c18880
Ilan Cohen <sanity@gmail.com>
2014-01-21 21:34:01 +0200

    added mean

--- server/server.js
+++ server/server.js
@@ -1,20 +1,65 @@
-// Module dependencies.
-var express = require('express');
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var express = require('express'),
+    fs = require('fs'),
+    passport = require('passport'),
+    logger = require('mean-logger');
+
+/**
+ * Main application entry file.
+ * Please note that the order of loading is important.
+ */
+
+// Load configurations
+// Set the node enviornment variable if not set before
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+// Initializing system variables 
+var config = require('./config/config'),
+    auth = require('./config/middlewares/authorization'),
+    mongoose = require('mongoose');
+
+// Bootstrap db connection
+var db = mongoose.connect(config.db);
+
+// Bootstrap models
+var models_path = __dirname + '/app/models';
+var walk = function(path) {
+    fs.readdirSync(path).forEach(function(file) {
+        var newPath = path + '/' + file;
+        var stat = fs.statSync(newPath);
+        if (stat.isFile()) {
+            if (/(.*)\.(js$|coffee$)/.test(file)) {
+                require(newPath);
+            }
+        } else if (stat.isDirectory()) {
+            walk(newPath);
+        }
+    });
+};
+walk(models_path);
+
+// Bootstrap passport config
+require('./config/passport')(passport);
 
 var app = express();
 
-// Configuration
-app.configure( function() {
-});
+// Express settings
+require('./config/express')(app, passport, db);
 
-// Routes
-app.get('/', function(req, res) {
-	res.send('Hello World');
-	function execute(someFunction, value) {
-		someFunction(value);
-	}
+// Bootstrap routes
+require('./config/routes')(app, passport, auth);
 
-	execute(function(word){ console.log(word) }, "Hello");
-});
+// Start the app by listening on <port>
+var port = process.env.PORT || config.port;
+app.listen(port);
+console.log('Express app started on port ' + port);
 
-app.listen(3000);
+// Initializing logger
+logger.init(app, passport, mongoose);
+
+// Expose app
+exports = module.exports = app;
